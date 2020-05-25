1e2c8a2cd38bbe6e663ac3acba20f4f0eb14679a
luiz.pereira <luiz.pereira@koerber.digital>
2020-05-25 11:00:00 +0100

    ci: add resource list step

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>É NOIS QUEIROZ!!!!!!</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -122,8 +123,8 @@
 export default {
   name: "HelloWorld",
   props: {
-    msg: String
-  }
+    msg: String,
+  },
 };
 </script>
 
